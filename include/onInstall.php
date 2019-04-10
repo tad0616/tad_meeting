0c4f90772220d42a9d3169ea135cf1d44090567e
mambax7 <mambax7@gmail.com>
2019-04-10 11:02:50 -0400

    utility
    Signed-off-by: mambax7 <mambax7@gmail.com>

--- include/onInstall.php
+++ include/onInstall.php
@@ -17,30 +17,17 @@
  * @version    $Id $
  **/
 
+use XoopsModules\Tad_meeting\Utility;
+
+include dirname(__DIR__) . '/preloads/autoloader.php';
 
 function xoops_module_install_tad_meeting(&$module)
 {
 
-    tad_meeting_mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_meeting");
-    tad_meeting_mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_meeting/file");
-    tad_meeting_mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_meeting/image");
-    tad_meeting_mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_meeting/image/.thumbs");
+    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_meeting");
+    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_meeting/file");
+    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_meeting/image");
+    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_meeting/image/.thumbs");
 
     return true;
-}
-
-//建立目錄
-function tad_meeting_mk_dir($dir = "")
-{
-    //若無目錄名稱秀出警告訊息
-    if (empty($dir)) {
-        return;
-    }
-
-    //若目錄不存在的話建立目錄
-    if (!is_dir($dir)) {
-        umask(000);
-        //若建立失敗秀出警告訊息
-        mkdir($dir, 0777);
-    }
 }
--- include/onUpdate.php
+++ include/onUpdate.php
@@ -17,123 +17,19 @@
  * @version    $Id $
  **/
 
+use XoopsModules\Tad_meeting\Utility;
+
 function xoops_module_update_tad_meeting($module, $old_version)
 {
     global $xoopsDB;
 
     //新增檔案欄位
-    if (chk_fc_tag()) {
-        go_fc_tag();
+    if (Utility::chk_fc_tag()) {
+        Utility::go_fc_tag();
     }
 
     return true;
 }
-//新增檔案欄位
-function chk_fc_tag()
-{
-    global $xoopsDB;
-    $sql    = "SELECT count(`tag`) FROM " . $xoopsDB->prefix("tad_meeting_files_center");
-    $result = $xoopsDB->query($sql);
-    if (empty($result)) {
-        return true;
-    }
-
-    return false;
-}
-
-function go_fc_tag()
-{
-    global $xoopsDB;
-    $sql = "ALTER TABLE " . $xoopsDB->prefix("tad_meeting_files_center") . "
-    ADD `upload_date` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '上傳時間',
-    ADD `uid` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT 0 COMMENT '上傳者',
-    ADD `tag` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '註記'
-    ";
-    $xoopsDB->queryF($sql) or redirect_header(XOOPS_URL . "/modules/system/admin.php?fct=modulesadmin", 30, $xoopsDB->error());
-}
 
 
 
-//建立目錄
-if (!function_exists('mk_dir')) {
-    function mk_dir($dir = "")
-    {
-        //若無目錄名稱秀出警告訊息
-        if (empty($dir)) {
-            return;
-        }
-
-        //若目錄不存在的話建立目錄
-        if (!is_dir($dir)) {
-            umask(000);
-            //若建立失敗秀出警告訊息
-            mkdir($dir, 0777);
-        }
-    }
-}
-
-//拷貝目錄
-if (!function_exists('full_copy')) {
-    function full_copy($source = "", $target = "")
-    {
-        if (is_dir($source)) {
-            @mkdir($target);
-            $d = dir($source);
-            while (false !== ($entry = $d->read())) {
-                if ($entry == '.' || $entry == '..') {
-                    continue;
-                }
-
-                $Entry = $source . '/' . $entry;
-                if (is_dir($Entry)) {
-                    full_copy($Entry, $target . '/' . $entry);
-                    continue;
-                }
-                copy($Entry, $target . '/' . $entry);
-            }
-            $d->close();
-        } else {
-            copy($source, $target);
-        }
-    }
-}
-
-if (!function_exists('rename_win')) {
-    function rename_win($oldfile, $newfile)
-    {
-        if (!rename($oldfile, $newfile)) {
-            if (copy($oldfile, $newfile)) {
-                unlink($oldfile);
-                return true;
-            }
-            return false;
-        }
-        return true;
-    }
-}
-
-if (!function_exists('delete_directory')) {
-    function delete_directory($dirname)
-    {
-        if (is_dir($dirname)) {
-            $dir_handle = opendir($dirname);
-        }
-
-        if (!$dir_handle) {
-            return false;
-        }
-
-        while ($file = readdir($dir_handle)) {
-            if ($file != "." && $file != "..") {
-                if (!is_dir($dirname . "/" . $file)) {
-                    unlink($dirname . "/" . $file);
-                } else {
-                    delete_directory($dirname . '/' . $file);
-                }
-            }
-        }
-        closedir($dir_handle);
-        rmdir($dirname);
-        return true;
-    }
-}
