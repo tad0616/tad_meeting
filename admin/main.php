c27ae64710e4788d7a5371d748b3db27f7ce3e9f
Tad <tad0616@gmail.com>
2019-04-16 12:39:16 +0800

    取消 in_array() 嚴格模式

--- admin/main.php
+++ admin/main.php
@@ -88,7 +88,7 @@ function list_tad_meeting_cate_tree($def_tad_meeting_cate_sn = '')
     $result = $xoopsDB->query($sql) or web_error($sql, __FILE__, __LINE__);
     while (list($tad_meeting_cate_sn, $tad_meeting_cate_parent_sn, $tad_meeting_cate_title) = $xoopsDB->fetchRow($result)) {
         $font_style = $def_tad_meeting_cate_sn == $tad_meeting_cate_sn ? ", font:{'background-color':'yellow', 'color':'black'}" : '';
-        $open = in_array($tad_meeting_cate_sn, $path_arr, true) ? 'true' : 'false';
+        $open = in_array($tad_meeting_cate_sn, $path_arr) ? 'true' : 'false';
         $display_counter = empty($cate_count[$tad_meeting_cate_sn]) ? '' : " ({$cate_count[$tad_meeting_cate_sn]})";
         $data[] = "{ id:{$tad_meeting_cate_sn}, pId:{$tad_meeting_cate_parent_sn}, name:'{$tad_meeting_cate_title}{$display_counter}', url:'main.php?tad_meeting_cate_sn={$tad_meeting_cate_sn}', open: {$open} ,target:'_self' {$font_style}}";
     }
@@ -128,7 +128,7 @@ function get_tad_meeting_cate_path($the_tad_meeting_cate_sn = '', $include_self 
             WHERE t1.tad_meeting_cate_parent_sn = '0'";
         $result = $xoopsDB->query($sql) or web_error($sql, __FILE__, __LINE__);
         while ($all = $xoopsDB->fetchArray($result)) {
-            if (in_array($the_tad_meeting_cate_sn, $all, true)) {
+            if (in_array($the_tad_meeting_cate_sn, $all)) {
                 //$main.="-";
                 foreach ($all as $tad_meeting_cate_sn) {
                     if (!empty($tad_meeting_cate_sn)) {
@@ -186,21 +186,21 @@ function get_tad_meeting_cate_options($page = '', $mode = 'edit', $default_tad_m
     $main = '';
     while (list($tad_meeting_cate_sn, $tad_meeting_cate_title) = $xoopsDB->fetchRow($result)) {
         // $tad_meeting_post = $moduleperm_handler->getGroupIds("tad_meeting_post", $tad_meeting_cate_sn, $mod_id);
-        if (!$isAdmin and !in_array($tad_meeting_cate_sn, $post_cate_arr, true)) {
+        if (!$isAdmin and !in_array($tad_meeting_cate_sn, $post_cate_arr)) {
             continue;
         }
 
         if ('edit' === $mode) {
             $selected = ($tad_meeting_cate_sn == $default_tad_meeting_cate_parent_sn) ? 'selected=selected' : '';
             $selected .= ($tad_meeting_cate_sn == $default_tad_meeting_cate_sn) ? 'disabled=disabled' : '';
-            $selected .= (in_array($level, $unselect, true)) ? 'disabled=disabled' : '';
+            $selected .= (in_array($level, $unselect)) ? 'disabled=disabled' : '';
         } else {
             if (is_array($default_tad_meeting_cate_sn)) {
-                $selected = in_array($tad_meeting_cate_sn, $default_tad_meeting_cate_sn, true) ? 'selected=selected' : '';
+                $selected = in_array($tad_meeting_cate_sn, $default_tad_meeting_cate_sn) ? 'selected=selected' : '';
             } else {
                 $selected = ($tad_meeting_cate_sn == $default_tad_meeting_cate_sn) ? 'selected=selected' : '';
             }
-            $selected .= (in_array($level, $unselect, true)) ? 'disabled=disabled' : '';
+            $selected .= (in_array($level, $unselect)) ? 'disabled=disabled' : '';
         }
         if ('none' === $page or empty($count[$tad_meeting_cate_sn])) {
             $counter = '';
@@ -360,7 +360,7 @@ function update_tad_meeting_cate($tad_meeting_cate_sn = '')
     $myts = MyTextSanitizer::getInstance();
 
     $tad_meeting_cate_sn = (int) $_POST['tad_meeting_cate_sn'];
-    $tad_meeting_cate_parent_sn = (int)$_POST['tad_meeting_cate_parent_sn'];
+    $tad_meeting_cate_parent_sn = (int) $_POST['tad_meeting_cate_parent_sn'];
     $tad_meeting_cate_title = $myts->addSlashes($_POST['tad_meeting_cate_title']);
     $tad_meeting_cate_desc = $myts->addSlashes($_POST['tad_meeting_cate_desc']);
     $tad_meeting_cate_sort = (int) $_POST['tad_meeting_cate_sort'];
